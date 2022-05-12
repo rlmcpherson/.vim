df3b293867a7d505697ddbad47d5af4fa7637434
Randall McPherson <randall@mcpherson.io>
2022-05-12 06:19:20 +0000

    debugging

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 mkdir $HOME/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-vim +GoInstallBinaries +qall
-$HOME/.vim/bundle/YouCompleteMe/install.py
+#vim +PluginInstall +qall
+#vim +GoInstallBinaries +qall
+#$HOME/.vim/bundle/YouCompleteMe/install.py
 
 # copy binaries to YouCompleteMe bin dir
 #mkdir ~/.vim/bundle/YouCompleteMe/third_party/go

9d0868185a8099236f1d77c1ddd87df6e8883e79
Randall McPherson <randall@mcpherson.io>
2022-05-12 01:50:37 +0000

    install YCM and copy go binaries

--- init.sh
+++ init.sh
@@ -3,5 +3,9 @@
 mkdir $HOME/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
-#$HOME/.vim/bundle/YouCompleteMe/install.py
 vim +GoInstallBinaries +qall
+$HOME/.vim/bundle/YouCompleteMe/install.py
+
+# copy binaries to YouCompleteMe bin dir
+mkdir ~/.vim/bundle/YouCompleteMe/third_party/go
+cp -r ~/bin ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/

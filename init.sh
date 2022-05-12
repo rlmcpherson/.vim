095fdfdf955a36f793a56ff48bf7b90adf5c8381
Randall McPherson <randall@mcpherson.io>
2022-05-12 15:19:33 +0000

    re-enable script

--- init.sh
+++ init.sh
@@ -2,10 +2,10 @@
 
 mkdir $HOME/.vim/bundle
 git clone https://github.com/gmarik/Vundle.vim ~/.vim/bundle/Vundle.vim
-#vim +PluginInstall +qall
-#vim +GoInstallBinaries +qall
-#$HOME/.vim/bundle/YouCompleteMe/install.py
+vim +PluginInstall +qall &>/dev/null
+vim +GoInstallBinaries +qall &>/dev/null
+$HOME/.vim/bundle/YouCompleteMe/install.py
 
 # copy binaries to YouCompleteMe bin dir
-#mkdir ~/.vim/bundle/YouCompleteMe/third_party/go
-#cp -r ~/bin ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/
+mkdir ~/.vim/bundle/YouCompleteMe/third_party/go
+cp -r ~/bin ~/.vim/bundle/YouCompleteMe/third_party/ycmd/third_party/go/
